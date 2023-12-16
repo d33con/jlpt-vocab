973b9c965ec8c3e5f5369b17f79671c6ae184f48
Oli Bullen <d33con@gmail.com>
2023-12-16 17:35:20 +0000

    set up test fucntionality

--- pages/test.tsx
+++ pages/test.tsx
@@ -4,15 +4,16 @@ import Word, { WordProps } from "../components/Word";
 import TestCompleted from "../components/TestCompleted";
 
 const Test = () => {
-  const [words, setWords] = useState<WordProps[]>([]);
+  const [dontKnowWords, setDontKnowWords] = useState<WordProps[]>([]);
+  const [knownWords, setKnownWords] = useState<WordProps[]>([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [currentPosition, setCurrentPosition] = useState(0);
+  const [currentArrayPosition, setCurrentArrayPosition] = useState(0);
 
   useEffect(() => {
     fetchMyWords();
   }, []);
 
-  function shuffleWords(words: WordProps[]): WordProps[] {
+  function shuffleWords(words: WordProps[]) {
     const shuffled = [...words];
 
     for (let i = shuffled.length - 1; i > 0; i--) {
@@ -27,21 +28,24 @@ const Test = () => {
     const res = await fetch("api/words");
     const data = await res.json();
     const shuffledWords = shuffleWords(data.words);
-    setWords(shuffledWords);
+    setDontKnowWords(shuffledWords);
     setIsLoading(false);
   }
 
-  function showNextWord() {
-    currentPosition > 0
-      ? setCurrentPosition(0)
-      : setCurrentPosition(currentPosition + 1);
+  function showNextWord(wordStatus?: string) {
+    wordStatus === "known"
+      ? setCurrentArrayPosition(0)
+      : setCurrentArrayPosition(currentArrayPosition + 1);
   }
 
-  function knownWord(word) {
-    const remainingWords = words.filter(
-      (testingWords) => word !== testingWords
-    );
-    setWords(remainingWords);
+  function handleKnownWord() {
+    const knownWord = dontKnowWords.shift();
+    setDontKnowWords(dontKnowWords);
+    setKnownWords((prevState) => prevState.concat(knownWord));
+    showNextWord("known");
+  }
+
+  function handleDontKnowWord() {
     showNextWord();
   }
 
@@ -59,25 +63,33 @@ const Test = () => {
       <main className="text-center">
         <p className="text-center text-2xl mb-8">Test</p>
         <div className="flex flex-col items-center justify-center w-full h-max">
-          {words.length === 0 ? (
+          {dontKnowWords.length === 0 ? (
             <TestCompleted restartTest={fetchMyWords} />
           ) : (
             <>
-              <p className="mb-4">Words left in this test: {words.length}</p>
-              <Word word={words[currentPosition]} />
+              <p className="mb-4">
+                Words left in this test: {dontKnowWords.length}
+              </p>
+              <Word word={dontKnowWords[currentArrayPosition]} />
               <div className="flex justify-around mt-12 w-1/4">
-                <button
-                  onClick={() => knownWord(words[currentPosition])}
-                  className="bg-white hover:bg-sky-100 text-sky-800 font-semibold py-2 px-4 border border-sky-400 rounded shadow"
-                >
-                  I know it
-                </button>
-                <button
-                  onClick={() => showNextWord()}
-                  className="bg-white hover:bg-sky-100 text-sky-800 font-semibold py-2 px-4 border border-sky-400 rounded shadow"
-                >
-                  I don't know it
-                </button>
+                <div className="flex flex-col">
+                  <div className="pb-2 italic">{knownWords.length}</div>
+                  <button
+                    onClick={() => handleKnownWord()}
+                    className="bg-white hover:bg-sky-100 text-sky-800 font-semibold py-2 px-4 border border-sky-400 rounded shadow"
+                  >
+                    I know it
+                  </button>
+                </div>
+                <div className="flex flex-col">
+                  <div className="pb-2 italic">{dontKnowWords.length}</div>
+                  <button
+                    onClick={() => handleDontKnowWord()}
+                    className="bg-white hover:bg-sky-100 text-sky-800 font-semibold py-2 px-4 border border-sky-400 rounded shadow"
+                  >
+                    I don't know it
+                  </button>
+                </div>
               </div>
             </>
           )}
