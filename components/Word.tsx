a28c960c9805e90aff1442bf42c6a20c464ce57d
Oli Bullen <d33con@gmail.com>
2023-12-19 17:57:22 +0000

    word card styling

--- components/Word.tsx
+++ components/Word.tsx
@@ -18,7 +18,7 @@ const Word: React.FC<{ word: WordProps }> = ({ word }) => {
   return (
     <div className="w-1/4 h-1/4 rounded overflow-hidden shadow-md p-16 text-center bg-slate-200">
       <p
-        className={`text-lg text-gray-700 mb-1 ${
+        className={`text-lg text-gray-700 mb-2 ${
           status ? "visible" : "invisible"
         } ${!word.furigana && "p-4"}`}
       >
@@ -26,15 +26,18 @@ const Word: React.FC<{ word: WordProps }> = ({ word }) => {
       </p>
       <p className="text-5xl text-gray-900 mb-8">{word?.word}</p>
       <p
-        className={`text-2xl text-gray-700 mb-4 ${
+        className={`text-xl text-gray-700 mb-4 h-16 ${
           showMeaning ? "visible" : "invisible"
         }`}
+        title={word?.meaning}
       >
-        {word?.meaning}
+        {word?.meaning.length > 35
+          ? word.meaning.slice(0, 35).concat("...")
+          : word.meaning}
       </p>
       <button
         onClick={() => setShowMeaning(!showMeaning)}
-        className="bg-white hover:bg-sky-100 text-sky-800 font-semibold py-2 px-4 border border-sky-400 rounded shadow"
+        className="bg-white hover:bg-sky-100 text-sky-800 text-sm py-1 px-2 border border-sky-400 rounded shadow"
       >
         Show meaning
       </button>
