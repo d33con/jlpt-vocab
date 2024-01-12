88e6a0ef71b503a8ede85c3e7f4f58e65a0a2707
Oli Bullen <d33con@gmail.com>
2024-01-12 15:52:06 +0000

    setup react-query correctly to avoid data leaks

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,22 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
+import { useState } from "react";
 import "tailwindcss/tailwind.css";
-import "../styles/globals.css";
 import FuriganaProvider from "../contexts/furiganaContext";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "../styles/globals.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const queryClient = new QueryClient();
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
 
   return (
     <QueryClientProvider client={queryClient}>
