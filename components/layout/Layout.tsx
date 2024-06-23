7299e120fce25da5e950257c93059055a1379f76
Oli Bullen <d33con@gmail.com>
2024-06-23 12:46:56 +0100

    add title to layout

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -3,6 +3,7 @@ import React, { ReactNode } from "react";
 import { Toaster } from "react-hot-toast";
 import DrawerMenu from "../navigation/DrawerMenu";
 import Header from "../navigation/Header";
+import Head from "next/head";
 
 const sora = Sora({
   subsets: ["latin"],
@@ -14,30 +15,37 @@ type Props = {
 };
 
 const Layout = (props: Props) => (
-  <main className={`${sora.variable} font-sans h-full min-h-screen bg-teal-50`}>
-    <div className="drawer">
-      <input id="drawer-menu" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content">
-        {/* Navbar */}
-        <Header />
-        {/* Page content */}
-        <div className="p-8">{props.children}</div>
+  <>
+    <Head>
+      <title>JLPT Vocab App: Practice Japanese vocabulary</title>
+    </Head>
+    <div
+      className={`${sora.variable} font-sans h-full min-h-screen bg-teal-50`}
+    >
+      <div className="drawer">
+        <input id="drawer-menu" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+          {/* Navbar */}
+          <Header />
+          {/* Page content */}
+          <main className="p-8">{props.children}</main>
+        </div>
+        <nav className="drawer-side z-20">
+          <label
+            htmlFor="drawer-menu"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          {/* Sidebar menu */}
+          <DrawerMenu />
+        </nav>
       </div>
-      <div className="drawer-side z-20">
-        <label
-          htmlFor="drawer-menu"
-          aria-label="close sidebar"
-          className="drawer-overlay"
-        ></label>
-        {/* Sidebar menu */}
-        <DrawerMenu />
-      </div>
+      <Toaster
+        position="bottom-center"
+        toastOptions={{ success: { duration: 4000 } }}
+      />
     </div>
-    <Toaster
-      position="bottom-center"
-      toastOptions={{ success: { duration: 4000 } }}
-    />
-  </main>
+  </>
 );
 
 export default Layout;
